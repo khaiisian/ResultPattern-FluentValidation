b39853e63f1df40dd3cddb1c4623cce5f6c152a7
Khai Sian Taang <khai.onthego@outlook.com>
2026-06-21 22:01:38 +0630

    Add project files.

---
+++ ResultPattern-FluentValidation.Db/AppDbContextModels/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ResultPattern_FluentValidation.Db.AppDbContextModels;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblItem> TblItems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=OTGEXPERTBOOKB5\\SQLEXPRESS;Database=Workspace;User ID=sa;Password=123;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblItem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Tbl_Item__3214EC07EF0D8E5C");
+
+            entity.ToTable("Tbl_Item");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ResultPattern-FluentValidation.Db/AppDbContextModels/TblItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ResultPattern_FluentValidation.Db.AppDbContextModels;
+
+public partial class TblItem
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Qty { get; set; }
+
+    public decimal Price { get; set; }
+}
